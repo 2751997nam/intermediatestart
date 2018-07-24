46041cf01ee6b39a2fa03630fd08ba87f7abd0f0
2751997nam <nambcvt275@gmail.com>
2018-07-24 14:57:32 +0700

    install laravel collective and make auth

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="{{ App::getLocale() }}">
+    <head>
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @yield('css')
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+        <script src="{{ asset('js/app.js') }}"></script>
+        @yield('script')
+    </body>
+</html>
